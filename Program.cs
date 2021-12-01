17e67c9e9f07095eada94fbaeced75047ccfc597
Jesse Varis <jessevaris@hotmail.com>
2021-12-01 22:45:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User, Welcome to ConsoleX.");
+        }
+    }
+}
