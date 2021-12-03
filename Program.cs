1d487566e4972852d61d4b4c336e827f200059e3
Jesse Varis <jessevaris@hotmail.com>
2021-12-03 11:44:43 +0100

    Added MainMeny

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace ConsoleX
         static void Main(string[] args)
         {
             Console.WriteLine("Hello User, Welcome to ConsoleX.");
+            Console.WriteLine("This is the main meny where you may choose ");
+            Console.WriteLine("one of the options from 0 - 2.");
+            MainMeny();
+
+
+
+            // Print out all the options for user.
+            //Console.ReadLine();
+
         }
+        public static void MainMeny()
+        {
+            Console.WriteLine("0_Switch");
+            Console.WriteLine("1_Indefinit Loop");
+            Console.WriteLine("2_Menu");
+        }
+
     }
 }
