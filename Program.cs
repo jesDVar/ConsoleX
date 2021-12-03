51f7e8588b12f7b061ac8fc28e84d643a8f6ded0
Jesse Varis <jessevaris@hotmail.com>
2021-12-03 13:02:19 +0100

    Added menu 3

--- Program.cs
+++ Program.cs
@@ -4,12 +4,21 @@ namespace ConsoleX
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello User, Welcome to ConsoleX.");
             Console.WriteLine("This is the main meny where you may choose ");
-            Console.WriteLine("one of the menus from 1 - 2.");
+            Console.WriteLine("one of the menus from 1 - 3.");
+
             MainMeny();
+
+            var input = Console.ReadLine();
+            switch (input) 
+            {
+                case "1":
+                    MenuOne
+            }
             
 
 
@@ -22,7 +31,10 @@ namespace ConsoleX
         {
             Console.WriteLine("Menu 1");
             Console.WriteLine("Menu 2");
+            Console.WriteLine("Menu 3");
         }
+        public static object MenuOne { get; private set; }
+
 
         private static void UserInput()
         { 
